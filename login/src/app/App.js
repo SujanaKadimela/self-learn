3dee623056ec1cb40325b161269cd3ae789ed1ed
KADIMELA Sujana <sujana.kadimela@hexagon.com>
2019-01-09 17:46:20 +0530

    login created

---
+++ login/src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Login } from '../modules/login/login';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <Login />;
+  }
+}
+
+export default App;
